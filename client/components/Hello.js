d69cd3dc9b05a21779e74f582485c127a7dc1208
jt wang <jtjtjtjtw@gmail.com>
2018-03-12 20:25:32 -0700

    initial structure

---
+++ client/components/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+const Hello = () => {
+  <div>
+    <h1>Hi</h1>
+  </div>
+}
+
+export default connect(null, null)(Hello)
--- client/routes.js
+++ client/routes.js
@@ -21,14 +21,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        {
-          isLoggedIn &&
-            <Switch>
-              {/* Routes placed here are only available after logging in */}
-              <Route path="/home" component={UserHome} />
-            </Switch>
-        }
-        {/* Displays our Login component as a fallback */}
+        {/* Fallback component */}
         <Route component={Login} />
       </Switch>
     )
