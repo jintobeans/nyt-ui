109b7c362ef7c6cd946177f4e523aac30977371e
jt wang <jtjtjtjtw@gmail.com>
2018-03-15 13:13:33 -0700

    added styles

--- client/components/Main.js
+++ client/components/Main.js
@@ -9,6 +9,7 @@ export class Main extends Component {
         <h1>New York Times Bestsellers</h1>
         <p>Authoritatively ranked lists of books sold in the United States, sorted by format and genre.
         </p>
+        <hr noshade />
         {children}
       </div>
     )
