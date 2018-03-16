01865fa9a3285afa92d7b96130778c0be5bb6929
jt wang <jtjtjtjtw@gmail.com>
2018-03-16 10:53:34 -0700

    button links clickable

--- client/components/BookItemButton.js
+++ client/components/BookItemButton.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react'
 
 export class BookItemButton extends Component {
-  handleClickDropdown = (e) => {
+
+  handleOpenLinks = (e) => {
     e.preventDefault()
-    let {links} = this.props
-    //this will allow user to click to show or hide buy links
-    let parentButtonDiv = document.getElementById(this.props.id)
-    if ( parentButtonDiv.childElementCount > 1 ){
-      parentButtonDiv.removeChild(parentButtonDiv.childNodes[1])
-    } else {
-      //show dropdown with buy links
-      let dropdown = document.createElement('div')
-      document.getElementById(this.props.id).appendChild(dropdown)
-      dropdown.id = 'buy-links'
-      let reducerCreateHTML = (accum, current) => {
-        return accum + `<div><a href=${current.url}>${current.name}</a></div>`
-      }
-      let htmlLinks = links.reduce(reducerCreateHTML, '')
-      dropdown.innerHTML = htmlLinks
-    }
+    document.getElementById(`buy${this.props.id}`).classList.toggle("showBuyLinks");
   }
 
   render() {
@@ -26,11 +12,20 @@ export class BookItemButton extends Component {
     return (
       <div className={`button${type}`} id={id}>
         <button
-        onClick={type === 'buy' ? this.handleClickDropdown : undefined}
-        label={label}
-        type={type}>
+          onClick={type === 'buy' ? this.handleOpenLinks : undefined}
+          label={label}
+          type={type}>
           {this.props.label}
         </button>
+        {type === 'buy'
+          ? <div id={`buy${id}`} className="buyDropdownContent">
+              {links.map((link) => {
+                return (
+                  <a href={link.url}>{link.name}</a>
+                )
+              })}
+            </div>
+          : undefined}
       </div>
     )
   }
